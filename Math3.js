8bbb096ba2946811d17c417bcae0a3bcaa15fe48
ravibarwad1818 <108672879+ravibarwad1818@users.noreply.github.com>
2022-09-28 22:51:26 +0530

    Add files via upload

---
+++ Math3.js
@@ -0,0 +1,10 @@
+function areaRectangle(width, length)
+{
+    let area = width* length;
+    return area;
+}
+
+let width = 5;
+let length = 6;
+
+document.write("Area =" + areaRectangle(width. length));
